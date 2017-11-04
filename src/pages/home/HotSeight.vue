687129cdd823e54ecac37e479c9e80503aab393f
lsl <931966061@qq.com>
2017-11-04 20:17:18 +0800

    lsl

--- src/pages/home/HotSeight.vue
+++ src/pages/home/HotSeight.vue
@@ -3,8 +3,8 @@
 		<h2 class="mp-modtitle">热销推荐</h2>
 		<div class="mp-hot-con">
 			<ul mp-role="hotList">
-				<li class="mp-hot-prod mp-border-bottom" data-click="ts_hot_recommend" data-click-from-index="0" data-click-from-value="故宫" data-click-dist-city="北京">
-					<a href="/touch/detail_989946426.html?cat=from_area%3Dts_hot_recommend%26from_index%3D0%26from_value%3D%25E6%2595%2585%25E5%25AE%25AB%26dist_city%3D%25E5%258C%2597%25E4%25BA%25AC" title="故宫" mp-role="analytics" data-params="from_area=ts_hot_recommend&amp;from_index=0&amp;from_value=故宫&amp;dist_city=北京">
+				<li class="mp-hot-prod mp-border-bottom">
+					<a href="#" title="故宫">
 						<div class="mp-hotlist-img image-ready">
 						<img src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg" alt="故宫">
 					</div>
@@ -27,8 +27,8 @@
 					</div>
 					</a>
 				</li>
-				<li class="mp-hot-prod mp-border-bottom" data-click="ts_hot_recommend" data-click-from-index="1" data-click-from-value="八达岭长城" data-click-dist-city="北京">
-					<a href="/touch/detail_777857128.html?cat=from_area%3Dts_hot_recommend%26from_index%3D1%26from_value%3D%25E5%2585%25AB%25E8%25BE%25BE%25E5%25B2%25AD%25E9%2595%25BF%25E5%259F%258E%26dist_city%3D%25E5%258C%2597%25E4%25BA%25AC" title="八达岭长城" mp-role="analytics" data-params="from_area=ts_hot_recommend&amp;from_index=1&amp;from_value=八达岭长城&amp;dist_city=北京">
+				<li class="mp-hot-prod mp-border-bottom">
+					<a href="#" title="八达岭长城" >
 						<div class="mp-hotlist-img image-ready">
 							<img src="http://img1.qunarzz.com/sight/p0/1708/2b/2b3b94de99c0a425a3.img.jpg_140x140_97813766.jpg" alt="八达岭长城">
 						</div>
@@ -50,8 +50,8 @@
 						</div>
 					</a>
 				</li>
-				<li class="mp-hot-prod mp-border-bottom" data-click="ts_hot_recommend" data-click-from-index="2" data-click-from-value="颐和园" data-click-dist-city="北京">
-					<a href="/touch/detail_4268080051.html?cat=from_area%3Dts_hot_recommend%26from_index%3D2%26from_value%3D%25E9%25A2%2590%25E5%2592%258C%25E5%259B%25AD%26dist_city%3D%25E5%258C%2597%25E4%25BA%25AC" title="颐和园" mp-role="analytics" data-params="from_area=ts_hot_recommend&amp;from_index=2&amp;from_value=颐和园&amp;dist_city=北京">
+				<li class="mp-hot-prod mp-border-bottom">
+					<a href="#" title="颐和园">
 						<div class="mp-hotlist-img image-ready">
 							<img src="http://img1.qunarzz.com/sight/p0/1505/d2/d274c92de14c93da.water.jpg_140x140_e20be8e0.jpg" alt="颐和园">
 						</div>
@@ -73,8 +73,8 @@
 						</div>
 					</a>
 				</li>
-				<li class="mp-hot-prod mp-border-bottom" data-click="ts_hot_recommend" data-click-from-index="3" data-click-from-value="北京欢乐谷" data-click-dist-city="北京">
-					<a href="/touch/detail_2769149828.html?cat=from_area%3Dts_hot_recommend%26from_index%3D3%26from_value%3D%25E5%258C%2597%25E4%25BA%25AC%25E6%25AC%25A2%25E4%25B9%2590%25E8%25B0%25B7%26dist_city%3D%25E5%258C%2597%25E4%25BA%25AC" title="北京欢乐谷" mp-role="analytics" data-params="from_area=ts_hot_recommend&amp;from_index=3&amp;from_value=北京欢乐谷&amp;dist_city=北京">
+				<li class="mp-hot-prod mp-border-bottom">
+					<a href="#" title="北京欢乐谷">
 						<div class="mp-hotlist-img image-ready">
 						<img src="http://img1.qunarzz.com/sight/p0/1508/a5/4003f9dd7bebf61eccbf64046e26d487.water.jpg_140x140_b05eb1df.jpg" alt="北京欢乐谷">
 					</div>
@@ -97,36 +97,37 @@
 					</div>
 					</a>
 				</li>
-				<li class="mp-hot-prod mp-border-bottom" data-click="ts_hot_recommend" data-click-from-index="4" data-click-from-value="慕田峪长城" data-click-dist-city="北京">
-					<a href="/touch/detail_1987315843.html?cat=from_area%3Dts_hot_recommend%26from_index%3D4%26from_value%3D%25E6%2585%2595%25E7%2594%25B0%25E5%25B3%25AA%25E9%2595%25BF%25E5%259F%258E%26dist_city%3D%25E5%258C%2597%25E4%25BA%25AC" title="慕田峪长城" mp-role="analytics" data-params="from_area=ts_hot_recommend&amp;from_index=4&amp;from_value=慕田峪长城&amp;dist_city=北京"><div class="mp-hotlist-img image-ready">
-						<img src="http://img1.qunarzz.com/sight/p0/1708/2b/2b6378fd3b2e1d86a3.img.jpg_140x140_eae81520.jpg" alt="慕田峪长城">
-					</div>
-					<div class="mp-hotlist-infos">
-						<div class="mp-hotlist-title">
-							慕田峪长城
+				<li class="mp-hot-prod mp-border-bottom">
+					<a href="#" title="慕田峪长城">
+						<div class="mp-hotlist-img image-ready">
+							<img src="http://img1.qunarzz.com/sight/p0/1708/2b/2b6378fd3b2e1d86a3.img.jpg_140x140_eae81520.jpg" alt="慕田峪长城">
 						</div>
-						<div class="mp-hotlist-desc">
-							秀美长城，关键是人少
+						<div class="mp-hotlist-infos">
+							<div class="mp-hotlist-title">
+								慕田峪长城
+							</div>
+							<div class="mp-hotlist-desc">
+								秀美长城，关键是人少
+							</div>
 						</div>
-					</div>
-					<div class="mp-price mp-hotlist-price">
-						¥
-						<em class="mp-price-num">
-							19.1
-						</em>
-						<span class="mp-price-text">
-							起
-						</span>
-					</div>
+						<div class="mp-price mp-hotlist-price">
+							¥
+							<em class="mp-price-num">
+								19.1
+							</em>
+							<span class="mp-price-text">
+								起
+							</span>
+						</div>
 					</a>
 				</li>
 			</ul>
-			div class="mp-modmore" data-click="ts_hot_recommend_more" data-click-dist-city="北京">
-			<a hybrid-link="" title="去哪儿门票" href="/touch/list_北京_北京.html?sort=pp&amp;cat=from_area%3Dts_hot_recommend_more%26dist_city%3D%25E5%258C%2597%25E4%25BA%25AC" mp-role="analytics" data-params="from_area=ts_hot_recommend_more&amp;dist_city=北京">
+			<div class="mp-modmore">
+			<a title="去哪儿门票" href="#">
 				查看所有产品
 			</a>
-		</div>
-	</div>
+		    </div>
+	    </div>
 	</div>
 </template>
 
