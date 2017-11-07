606e11de19db3cfec329ae535b768ee8b333aca7
yang <920526047@qq.com>
2017-11-07 09:52:51 +0800

    yyj

--- src/pages/activity/components/Topic.vue
+++ src/pages/activity/components/Topic.vue
@@ -18,6 +18,12 @@
                 <img class="mp-topic-img" alt="女孩" src="//s.qunarzz.com/piao_topic/image/touch/custom/2017/2390nationalday09/girl.gif" style="opacity:1">
             </div> 
         </div>
+        <div class="mp-city-outer" mp-role="cityOuter"> 
+            <div class="mp-city-container"> 
+            <ul class="mp-city-list mp-flexbox" mp-role="cityWrap"><li class="mp-city-item mp-flexbox-layout mp-city-actived" mp-role="cityItem" data-hotorder="1" data-contentid="2449"> <span class="mp-city-inner">北京</span> </li><li class="mp-city-item mp-flexbox-layout" mp-role="cityItem" data-hotorder="<2>" data-contentid="2409"> <span class="mp-city-inner">成都</span> </li><li class="mp-city-item mp-flexbox-layout" mp-role="cityItem" data-hotorder="<7>" data-contentid="2416"> <span class="mp-city-inner">重庆</span> </li><li class="mp-city-item mp-flexbox-layout" mp-role="cityItem" data-hotorder="<4>" data-contentid="2443"> <span class="mp-city-inner">广州</span> </li></ul> 
+            </div> 
+            <div class="mp-city-expand" mp-role="cityMoreBtn"></div> 
+        </div>
     </div>
 </template>
 <script>
@@ -48,7 +54,8 @@
     }
     .mp-banner {
         position: relative;
-        padding-bottom: 64.4%;
+        height: 0;
+        padding-bottom: 32.4%; 
         background-color: #f3cba1;
     }
     .mp-topic-img {
@@ -78,8 +85,8 @@
     .mp-banner-girl {
         position: absolute;
         z-index: 3;
-        right: -23%;
-        bottom: -5%;
+        right: -28%;
+        top: 10%;
         width: 100%;
     }
 </style>
